e682d6457228d7162fcef606c9965cc5c72428c7
Serapheim Dimitropoulos <serapheim.dimitropoulos@weka.io>
2025-10-09 10:57:27 -0400

    Allow open()/close() without context manager
    File registration/deregistration can become a bottleneck. Opening
    cuFile handles without a context manager allows us to re-use handles
    and register them once.

--- cufile/cufile.py
+++ cufile/cufile.py
@@ -49,37 +49,59 @@ class CuFile:
         if use_direct_io:
             self._os_mode |= os.O_DIRECT
 
-    def __enter__(self):
+        self._handle = None
+        self._cu_file_handle = None
+
+    def __del__(self):
         """
-        Context manager entry.
+        Destructor to ensure the file is closed.
         """
+        if self.is_open:
+            self.close()
+
+    @property
+    def is_open(self) -> bool:
+        return self._handle is not None
+
+    def open(self):
+        """Opens the file and registers the handle."""
+        if self.is_open:
+            return
         self._handle = os.open(self._path, self._os_mode)
         self._cu_file_handle = cuFileHandleRegister(self._handle)
+
+    def close(self):
+        """Deregisters the handle and closes the file."""
+        if not self.is_open:
+            return
+        cuFileHandleDeregister(self._cu_file_handle)
+        os.close(self._handle)
+        self._handle = None
+        self._cu_file_handle = None
+
+    def __enter__(self):
+        """Context manager entry."""
+        self.open()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        """
-        Context manager exit.
-        """
-        cuFileHandleDeregister(self._cu_file_handle)
-        os.close(self._handle)
+        """Context manager exit."""
+        self.close()
     
     def read(self, dest: ctypes.c_void_p, size: int, file_offset: int = 0, dev_offset: int = 0):
-        """
-        Read from the file.
-        """
+        """Read from the file."""
+        if not self.is_open:
+            raise IOError("File is not open.")
         return cuFileRead(self._cu_file_handle, dest, size, file_offset, dev_offset)
     
     def write(self, src: ctypes.c_void_p, size: int, file_offset: int = 0, dev_offset: int = 0):
-        """
-        Write to the file.
-        """
+        """Write to the file."""
+        if not self.is_open:
+            raise IOError("File is not open.")
         return cuFileWrite(self._cu_file_handle, src, size, file_offset, dev_offset)
 
     def get_handle(self):
-        """
-        Get the file handle.
-        """
+        """Get the file handle."""
         return self._handle
     
-    
+ 
