a30b0761af8b61539d2ddc9495e9d8419317b06d
Ilya Yanok <ilya.yanok@gmail.com>
2025-10-13 05:54:47 -0400

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cufile-python",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[],
     author="Ilya Yanok",
